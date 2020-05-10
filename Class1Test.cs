ddb117f47d568c25f4f74fbfb5d2917932fead0b
malhaz <karleonesvan@yandex.ru>
2020-05-10 23:46:59 +0300

    Add files via upload

---
+++ Class1Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Class1Test
+    {
+        DLL.Class1 class1 = new DLL.Class1();
+
+        [TestMethod]
+        public void CirleArea()
+        {
+            //входные данные
+            double x = 4;
+             
+            //ожидаемый результат
+            double expres = 16 * Math.PI;
+
+            //тест
+            double actual = class1.Area(x);
+
+            Assert.AreEqual(expres, actual);
+        }
+
+        [TestMethod]
+        public void RectArea()
+        {
+            //входные данные
+            double x = 3;
+            double y = 4;
+            double z = 5;
+
+            //ожидаемый результат
+            double expres = 6;
+
+            //тест
+            double actual = class1.Area(x,y,z);
+
+            Assert.AreEqual(expres, actual);
+        }
+
+        [TestMethod]
+        public void Rornot() 
+        {
+            //входные данные
+            double x = 3;
+            double y = 4;
+            double z = 5;
+
+            //ожидаемый результат
+            bool expres = true;
+
+            //тест
+            bool actual = class1.Rornot(x, y, z);
+
+            Assert.AreEqual(expres, actual);          
+        }
+    }
+}
